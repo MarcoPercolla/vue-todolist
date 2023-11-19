88b48c15474e75c7b376d9662b6de3076120d526
ReDMallard <marco@titori.it>
2023-11-19 20:28:35 +0100

    lista stampata in pagina

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
-console.log("ok")
+const { createApp } = Vue;
+
+const tasks = {
+    data: function () {
+        return {
+            toDolist: [
+                {
+                    text: "fare la spesa",
+                    done: false,
+                },
+                {
+                    text: "pulire la stanza",
+                    done: false,
+                },
+                {
+                    text: "andare in palestra",
+                    done: false,
+                },
+                {
+                    text: "preparare la cena",
+                    done: false,
+                },
+                {
+                    text: "portare fuori il cane",
+                    done: true,
+                },
+                {
+                    text: "fare l'esercitazione",
+                    done: false,
+                },
+            ],
+        }
+
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.log("ok");
+
+    },
+};
+
+createApp(tasks).mount('#app')
